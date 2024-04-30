cf9ed22603f041a2424c41c08bbeff7ff0a60085
NuraneIsali <isayeva.nurane97@gmail.com>
2024-04-30 10:25:20 +0400

    prompt test

---
+++ src/main.js
@@ -0,0 +1,17 @@
+ let x=prompt(
+  "Yaşadığınız Bölgəni qeyd edin"  
+);
+
+
+
+if(x=="Xetai"){
+
+
+console.log("Doğru Ünvandasınız...");
+
+
+} else{
+
+
+    console.log("Ünvan doğru deyil...");
+}
